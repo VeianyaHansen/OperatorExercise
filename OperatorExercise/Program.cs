5fed8e2f42783ce84824f4f98eca64970f312fa1
Veianya Hansen <melaya0105@gmail.com>
2021-05-05 08:13:37 -0700

    Add project files.

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var a = 17;
+            // var b = 4;
+
+            // var add = a + b;
+            // var sub = a - b;
+            // var mult = a * b; 
+            // var div = a / b;
+            // var mod = a % b;
+
+            // Console.WriteLine($"The sum is {add}, the difference is {sub}, and the product is {mult}.");
+            // Console.WriteLine();
+            // Console.WriteLine($"{a} / {b} is {div} remainder {mod}");
+            // Console.WriteLine();
+            Console.WriteLine("What is the area of the circle:");
+            var radius = double.Parse(Console.ReadLine());
+            var areaOfCircle = CalculateArea(radius);
+
+            Console.WriteLine($"The area of a circle with radius of {radius} is {areaOfCircle}");
+        }
+
+        public static double CalculateArea(double radius)
+        {
+            return Math.PI * (radius * radius);
+        }
+    }
+}
